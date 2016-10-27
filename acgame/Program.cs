3f81e4d9ecf2d3fe7ff11c42159787d4730cf8a4
Hattomotto <hattomotto8810@gmail.com>
2016-10-27 22:14:37 +0900

    Initial Commit

---
+++ acgame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //altseedの初期化
+            asd.Engine.Initialize("Empty", 960, 640, new asd.EngineOption());
+
+            asd.TextureObject2D player = new asd.TextureObject2D();
+
+            player.Texture = asd.Engine.Graphics.CreateTexture2D("Resources/playerfly.png");
+
+            asd.Engine.AddObject2D(player);
+
+            while (asd.Engine.DoEvents()) {
+
+                if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Escape) == asd.KeyState.Push)
+                    break;
+
+                asd.Engine.Update();
+            }
+
+              
+            asd.Engine.Terminate();
+        }
+    }
+}
---
+++ acgame/testscene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game1
+{
+    class TestScene : asd.Scene
+    {
+        protected override void OnRegistered()
+        {
+            
+        }
+    }
+}
